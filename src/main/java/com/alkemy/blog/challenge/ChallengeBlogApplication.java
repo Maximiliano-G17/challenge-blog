008315ac607ed44ab2e05dab67172af0d13caa3b
Maximiliano-G17 <maxigimenez1792@gmail.com>
2021-04-19 15:46:20 -0300

    empezando proyecto

---
+++ src/main/java/com/alkemy/blog/challenge/ChallengeBlogApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.blog.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/blog/challenge/ChallengeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.blog.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
